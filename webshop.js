9a02d0ff5e7e54a5a7cd8726a26ce01556f63cb1
Alexander <alex143r@stud.kea.dk>
2021-05-19 08:26:30 +0200

    mobile webshop caroCurr

--- webshop.js
+++ webshop.js
@@ -3,5 +3,4 @@ document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".carousel");
   var options = { padding: 30, dist: 0 };
   var instances = M.Carousel.init(elems, options);
-  console.log(elems);
 });
