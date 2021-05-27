fc9a4f79b46f9bb84f0b1f06cc30224e9d50dfa1
Alexander <alex143r@stud.kea.dk>
2021-05-28 00:14:45 +0200

    build test

---
+++ dist/baf.4c8dbefc.js
@@ -0,0 +1,2 @@
+!function(){const t=document.querySelectorAll(".tab");for(let c of t)c.addEventListener("click",e);function e(){this.classList.add("selected")}}();
+//# sourceMappingURL=baf.4c8dbefc.js.map
---
+++ dist/webshop.818340cf.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelectorAll(".carousel");M.Carousel.init(e,{padding:30,dist:0})}));
+//# sourceMappingURL=webshop.818340cf.js.map
